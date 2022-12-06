17577feb6702a32f64b96e70fdab6410745b9b0a
spangen87 <rikard.spangmyr@bestlas.se>
2022-12-06 11:10:00 +0000

    feat: Add boilerplate html to index html and add static urls to render images

--- bls_digital/settings.py
+++ bls_digital/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
