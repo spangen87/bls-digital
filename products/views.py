02a79eaa52553eb692098941abc0212960b231d4
spangen87 <rikard.spangmyr@bestlas.se>
2022-12-08 15:24:08 +0000

    feat: Add template and view to products, and adjusted top padding for body

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """
+    A view to show alls products, including sorting and search
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
