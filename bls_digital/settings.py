52a8478038ee7bcd2273f24960b0036dd1b6dbc5
spangen87 <rikard.spangmyr@bestlas.se>
2022-12-09 09:10:36 +0000

    fix: Add context processor for media files and change links to images

--- bls_digital/settings.py
+++ bls_digital/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # requried by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
