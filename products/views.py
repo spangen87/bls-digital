fb47e2c6906ca9b0f635bde8f1d2da1f5be6a84d
spangen87 <rikard.spangmyr@bestlas.se>
2022-12-13 09:28:20 +0000

    feat: Add search for products in navbar

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
+
 
 def all_products(request):
     """
@@ -9,9 +12,22 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    result = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            result = request.GET['search']
+            if not result:
+                messages.error(
+                    request, "You need to enter something to search for.")
+                return redirect(reverse('products'))
+
+            search_results = Q(name__icontains=result) | Q(description__icontains=result)
+            products = products.filter(search_results)
 
     context = {
         'products': products,
+        'search_term': result,
     }
 
     return render(request, 'products/products.html', context)
