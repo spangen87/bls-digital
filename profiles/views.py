8fb58d55b36b2391adaeebf435d673f6c1156ce8
spangen87 <rikard.spangmyr@bestlas.se>
2022-12-30 13:25:16 +0000

    feat: Change template for success toast and save profile form

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -9,6 +10,13 @@ def profile(request):
     Display user profiles
     """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated!')
+
     template = 'profiles/profile.html'
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -16,6 +24,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
