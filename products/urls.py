9a990cdf9790fb968acd23e84e60a8a6cfbdbd54
spangen87 <rikard.spangmyr@bestlas.se>
2023-01-01 10:09:12 +0000

    feat: Add delete product functionality and links in product templates

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path(
+        'delete/<int:product_id>/',
+        views.delete_product,
+        name='delete_product'),
 ]
