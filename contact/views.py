0d72c2b4bbff52762a592239443363f0702a91d1
spangen87 <rikard.spangmyr@bestlas.se>
2023-01-01 11:44:30 +0000

    feat: Add so the contact form can send emails

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
 from .forms import ContactForm
 
 
@@ -10,7 +12,9 @@ def contact(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
-            send_email(name, email, message)
+            recipient = settings.DEFAULT_FROM_EMAIL
+            subject = f'BLS Digital contact form: {name}'
+            send_mail(subject, message, email, [recipient])
             return redirect('thank_you')
     else:
         form = ContactForm()
