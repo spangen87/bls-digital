4155f510e701e80fa205fec8800c67ab1e538ae6
spangen87 <rikard.spangmyr@bestlas.se>
2023-01-03 11:04:54 +0000

    fix: Remove commented out database from settings

--- bls_digital/settings.py
+++ bls_digital/settings.py
@@ -127,14 +127,6 @@ WSGI_APPLICATION = 'bls_digital.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
